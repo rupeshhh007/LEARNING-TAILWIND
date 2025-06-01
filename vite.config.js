c0e2314d70c06333a58b63b5e908b25f03131b04
Rupesh Tripathi <rupeshstripathi1234@gmail.com>
2025-06-02 00:12:32 +0530

    using vite

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+// You don't need to import Tailwind here manually when using PostCSS.
+// Tailwind runs via postcss.config.js.
+
+export default defineConfig({
+  // No plugins needed for basic Tailwind use
+})
